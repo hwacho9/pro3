cc47376621fe8d817b1d66a4f88c9c43ab1951e7
hwacho9 <63436365+hwacho9@users.noreply.github.com>
2023-10-01 23:33:17 +0900

    fix match.c

--- B1/match.c
+++ B1/match.c
@@ -4,34 +4,39 @@ int main()
 {
     int k, n, m;
 
-    // k, a, n, mの入力
-    fprintf(stderr, "整数 k を入力してください (1≤k≤10): ");
+    fprintf(stderr, "k: ");
+    fprintf(stdout, "\n");
     scanf("%d", &k);
 
     if (k < 1 || k > 10)
     {
         fprintf(stderr, "kの値が無効です。1から10の範囲内で指定してください。\n");
+        fprintf(stdout, "\n");
         return 1;
     }
 
     int a[10];
 
-    fprintf(stderr, "k 個の整数を入力してください: ");
+    fprintf(stderr, "%d個の整数: ", k);
+    fprintf(stdout, "\n");
     for (int i = 0; i < k; i++)
     {
         scanf("%d", &a[i]);
     }
 
-    fprintf(stderr, "整数 n を入力してください (k≤n≤16): ");
+    fprintf(stderr, "n: ");
+    fprintf(stdout, "\n");
     scanf("%d", &n);
 
     if (n < k || n > 16)
     {
         fprintf(stderr, "k以上16以下で指定してください。\n");
+        fprintf(stdout, "\n");
         return 1;
     }
 
-    fprintf(stderr, "整数 m を入力してください (1≤m≤16): ");
+    fprintf(stderr, "m : ");
+    fprintf(stdout, "\n");
     scanf("%d", &m);
 
     if (m < 1 || m > 16)
@@ -43,27 +48,28 @@ int main()
     // m 回の処理
     for (int j = 0; j < m; j++)
     {
-        int b[10];
-        fprintf(stderr, "n 個の整数を入力してください: ");
+        int b[16];
+        fprintf(stderr, "%d個の整数を入力してください: ", n);
         for (int i = 0; i < n; i++)
         {
             scanf("%d", &b[i]);
         }
 
-        // 条件を満たすかどうかの検証
-        int match = 1;
-        for (int i = 0; i < n - k + 1; i++)
+        int match = 0;
+        for (int i = 0; i <= n - k; i++) // 途中
         {
+            int isPattern = 1;
             for (int x = 0; x < k; x++)
             {
                 if (b[i + x] != a[x])
                 {
-                    match = 0;
+                    isPattern = 0;
                     break;
                 }
             }
-            if (match == 1)
+            if (isPattern == 1)
             {
+                match = 1;
                 break;
             }
         }
