12ece2a54ed74e46590d0fa2b2e8dff45dca2d64
hwacho9 <csh77776@gmail.com>
2023-11-02 23:58:59 +0900

    B5

---
+++ B5/5-1/array_scan.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *new_array_scan(int *pn)
+{
+    int *a = NULL;
+    // まず整数 n を読み込む
+    if (scanf("%d", pn) != 1)
+    {
+        // n の読み込みに失敗した場合
+        return NULL;
+    }
+
+    // サイズ n の int 型配列を割り当てる
+    a = (int *)malloc(sizeof(int) * *pn);
+    if (a == NULL)
+    {
+        // メモリ割り当てに失敗した場合
+        exit(1)
+    }
+
+    // n 個の整数を標準入力から配列に読み込む
+    for (int i = 0; i < *pn; i++)
+    {
+        if (scanf("%d", &a[i]) != 1)
+        {
+            // 整数の読み込みに失敗した場合
+            free(a); // メモリを解放してエラーを処理
+            return NULL;
+        }
+    }
+
+    return a;
+}
