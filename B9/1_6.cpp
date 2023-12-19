46cf47e750653b82702ebbe4f667833bd3217a98
hwacho9 <csh77776@gmail.com>
2023-12-19 21:10:25 +0900

    B9 fix

--- B9/1_6.cpp
+++ B9/1_6.cpp
@@ -12,22 +12,37 @@ public:
         real = 0.0;
         imag = 0.0;
     }
+
     Complex(double r, double i)
     {
         real = r;
         imag = i;
     }
+
+    // 新しいコンストラクタ
+    Complex(double r)
+    {
+        real = r;
+        imag = 0.0;
+    }
+
     Complex operator+(const Complex &other) const
     {
         double r = real + other.real;
         double i = imag + other.imag;
         return Complex(r, i);
     }
+
     ~Complex() {}
+
     double re() const { return real; }
+
     double im() const { return imag; }
+
     void set_re(double r) { real = r; }
+
     void set_im(double i) { imag = i; }
+
     void print(std::ostream &os) const
     {
         os << real << "+" << imag << "i";
@@ -40,13 +55,19 @@ int main(void)
     x = Complex(1.00, 2.00);
     y = Complex(2.22, 3.14);
     z = Complex(4.23, 9.99);
-    a = x + y + z;
+
+    // 新しいコンストラクタを使用してComplexオブジェクトを初期化
+    a = Complex(5.0);
+
     x.print(std::cout);
     std::cout << std::endl;
+
     y.print(std::cout);
     std::cout << std::endl;
+
     z.print(std::cout);
     std::cout << std::endl;
+
     a.print(std::cout);
     std::cout << std::endl;
 
